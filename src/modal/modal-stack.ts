71ff29549d01bfae19a40041a6b5a5824a7196e9
CoinCatEx <vcpcnus@gmail.com>
2021-08-13 12:29:15 +0100

    fixed backdrop

--- src/modal/modal-stack.ts
+++ src/modal/modal-stack.ts
@@ -58,7 +58,8 @@ export class NgbModalStack {
     const revertPaddingForScrollBar = this._scrollBar.compensate();
     const removeBodyClass = () => {
       if (!this._modalRefs.length) {
-        renderer.setStyle(this._document.body, 'overflow-y', 'hidden');
+        renderer.removeClass(this._document.body, 'modal-open');
+        renderer.setStyle(this._document.body, 'overflow-y', '');
         this._revertAriaHidden();
       }
     };
@@ -85,6 +86,7 @@ export class NgbModalStack {
 
     this._applyWindowOptions(windowCmptRef.instance, options);
     if (this._modalRefs.length === 1) {
+      renderer.addClass(this._document.body, 'modal-open');
       renderer.setStyle(this._document.body, 'overflow-y', 'hidden');
     }
 
